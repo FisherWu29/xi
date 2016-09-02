c6c55d3708826b6dea114a7d1cdd2ebfa684d1cd
maynard <1963789086>
2016-09-03 00:17:16 +0800

    删除一些错误

--- src/main/java/com/xi/dao/PersonDao.java
+++ src/main/java/com/xi/dao/PersonDao.java
@@ -4,9 +4,9 @@ import com.xi.bean.Person;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class PersonDao extends SqlSessionDaoSupport{
-        public Person getPersonByName(String name) {
-            return ((Person) this.getSqlSession().selectList("com.xi.bean.Person.getPersonByName", name));
-        }
-}
+//@Repository
+//public class PersonDao extends SqlSessionDaoSupport{
+////        public Person getPersonByName(String name) {
+////            return ((Person) this.getSqlSession().selectList("com.xi.bean.Person.getPersonByName", name));
+////        }
+//}
--- src/main/java/com/xi/service/PersonService.java
+++ src/main/java/com/xi/service/PersonService.java
@@ -1,18 +1,18 @@
 package com.xi.service;
 
 import com.xi.bean.Person;
-import com.xi.dao.PersonDao;
+//import com.xi.dao.PersonDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by QAQ on 2016/8/22.
  */
-@Service
-public class PersonService {
-    @Autowired
-    private PersonDao person;
-    public Person getPersonByName(String name) {
-        return (person.getPersonByName(name));
-    }
-}
+//@Service
+//public class PersonService {
+////    @Autowired
+////    private PersonDao person;
+////    public Person getPersonByName(String name) {
+////        return (person.getPersonByName(name));
+////    }
+//}
